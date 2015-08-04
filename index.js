c3c5547149fbe9bb760834b624f786dbd9d7cbc7
asakusuma <asa.kusuma@gmail.com>
2015-08-04 15:56:46 -0700

    Implement local config merging into main config

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /* jshint node: true */
 'use strict';
 
+var walkSync = require('walk-sync');
+var relative = require('require-relative');
+var merge = require('merge');
+
 module.exports = {
-  name: 'ember-local-config'
+  name: 'ember-local-config',
+  config: function (env, baseConfig) {
+    var combinedLocalConfig = {};
+
+    // Get the ember-addon field from the host app's package.json
+    var emberAddonConfig = require('./package')['ember-addon'];
+
+    // Set the default config path
+    var configPath = './config';
+
+    // Use the config path from host app's package.json if it exists
+    if (emberAddonConfig && emberAddonConfig.configPath) {
+      configPath = './' + emberAddonConfig.configPath;
+    }
+
+    walkSync(configPath).filter(function(path) {
+      // We only want to pull in files that end in .local.js
+      return path.split('/').pop().substr(-9) === '.local.js';
+    }).forEach(function(path) {
+      var fullPath = configPath + '/' + path;
+      var localConfig = relative(fullPath)(env);
+      merge(combinedLocalConfig, localConfig);
+    });
+
+    return combinedLocalConfig;
+  }
 };
