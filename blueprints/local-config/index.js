4eb8a99e7ab0a2ab6be9102aa114fca082de5215
asakusuma <asa.kusuma@gmail.com>
2015-08-04 17:10:33 -0700

    Support custom config paths in blueprint

--- blueprints/local-config/index.js
+++ blueprints/local-config/index.js
@@ -1,18 +1,16 @@
+var getConfigPath = require('./../../lib/utils').getConfigPath;
+
 module.exports = {
   description: 'A local config file',
   normalizeEntityName: function(entityName) {
     entityName = entityName || 'config';
     return entityName;
+  },
+  fileMapTokens: function(options) {
+    return {
+      __config__: function(options) {
+        return getConfigPath();
+      }
+    }
   }
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
 };
