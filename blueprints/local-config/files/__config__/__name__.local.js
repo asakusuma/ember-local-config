af9adbb32651ae83dba7522211e540a20d0fe01b
asakusuma <asa.kusuma@gmail.com>
2015-08-05 11:22:50 -0700

    Add usage instructions to config blueprint

--- blueprints/local-config/files/__config__/__name__.local.js
+++ blueprints/local-config/files/__config__/__name__.local.js
@@ -1,5 +1,21 @@
 /* jshint node: true */
 
+/* ==================
+ * USAGE INSTRUCTIONS
+ * ==================
+ *
+ * If you'd like to make a local change to this file, you need
+ * to tell git to ignore any changes to this file. You can do
+ * this by running:
+ *
+ * git update-index --skip-worktree [file-path]
+ *
+ * If you want to undo the ignore and actually commit changes:
+ *
+ * git update-index --no-skip-worktree [file-path]
+ *
+ */
+
 module.exports = function(environment) {
   return {};
 };
