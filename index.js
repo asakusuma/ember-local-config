a0fceaee50525f0e545c728ee18ca78e5ffda4dd
Theodore Ni <zyloch@gmail.com>
2016-05-11 09:14:06 -0700

    Read local config relative to project configPath.
    While this doesn't touch where the blueprint generates the config file,
    it hardens the behavior of the addon in custom directory structures.

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 /* jshint node: true */
 'use strict';
 
-var walkSync = require('walk-sync');
-var relative = require('require-relative');
 var merge = require('merge');
-var getConfigPath = require('./lib/utils').getConfigPath;
+var path = require('path');
+var relative = require('require-relative');
+var walkSync = require('walk-sync');
+
+function endsWith(base, suffix) {
+  return base.substr(-suffix.length) === suffix;
+}
 
 module.exports = {
   name: 'ember-local-config',
-  config: function (env, baseConfig) {
+  config: function(env, baseConfig) {
     var combinedLocalConfig = {};
 
-    var configPath = getConfigPath();
-
-    walkSync(configPath).filter(function(path) {
+    var configDirectory = path.dirname(this.project.configPath());
+    walkSync(configDirectory).filter(function(filePath) {
       // We only want to pull in files that end in .local.js
-      return path.split('/').pop().substr(-9) === '.local.js';
-    }).forEach(function(path) {
-      var fullPath = configPath + '/' + path;
+      return endsWith(path.basename(filePath), '.local.js');
+    }).forEach(function(filePath) {
+      var fullPath = path.join(configDirectory, filePath);
+      if (!path.isAbsolute(fullPath)) {
+        fullPath = '.' + path.sep + fullPath;
+      }
       var localConfig = relative(fullPath)(env);
       merge(combinedLocalConfig, localConfig);
     });
